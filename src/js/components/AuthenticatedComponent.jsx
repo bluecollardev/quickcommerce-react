bc87aef40de849e37234258369264836d549dc4f
Lucas Lopatka <lucas.lopatka@interdynamix.com>
2017-10-27 11:20:54 -0600

    Changes to AuthenticatedComponent - going to use context or something else to pass around app services.

--- src/js/components/AuthenticatedComponent.jsx
+++ src/js/components/AuthenticatedComponent.jsx
@@ -1,13 +1,9 @@
 import assign from 'object-assign'
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import LoginStore from '../stores/LoginStore.jsx'
 import UserStore from '../stores/UserStore.jsx'
-import CustomerStore from '../stores/CustomerStore.jsx'
-
-import Auth from '../services/AuthService.jsx'
-import CustomerService from '../services/CustomerService.jsx'
 
 // TODO: Split out customers, add logic to another HoC
 export default (ComposedComponent) => {
@@ -23,8 +19,7 @@ export default (ComposedComponent) => {
             
             this.state = assign({},
                 this.getLoginState(),
-                this.getUserState(),
-                this.getCustomerState())
+                this.getUserState())
         }
 
         getLoginState() {
@@ -37,32 +32,17 @@ export default (ComposedComponent) => {
         
         getUserState() {
             return {
+                userToken: LoginStore.userToken,
                 loggedIn: LoginStore.isLoggedIn(),
-                user: UserStore.user,
-                billingAddress: UserStore.billingAddress,
-                shippingAddress: UserStore.shippingAddress,
-                userToken: LoginStore.userToken
-            }
-        }
-        
-        getCustomerState() {
-            return {
-                loggedIn: LoginStore.isLoggedIn(),
-                customer: CustomerStore.customer,
-                billingAddress: CustomerStore.billingAddress,
-                billingAddressString: CustomerStore.billingAddressString,
-                shippingAddress: CustomerStore.shippingAddress,
-                shippingAddressString: CustomerStore.shippingAddressString,
-                customerToken: CustomerStore.customerToken
+                user: UserStore.user
             }
         }
         
         onChange() {
-            this.setState(
+			this.setState(
                 assign({},
                     this.getLoginState(),
-                    this.getUserState(),
-                    this.getCustomerState())
+                    this.getUserState())
             )
         }
 
@@ -70,29 +50,22 @@ export default (ComposedComponent) => {
             this.changeListener = this.onChange.bind(this)
             LoginStore.addChangeListener(this.changeListener)
             UserStore.addChangeListener(this.changeListener)
-            CustomerStore.addChangeListener(this.changeListener)
         }
         
         componentWillUnmount() {
             if (typeof this.changeListener === 'function') {
                 LoginStore.removeChangeListener(this.changeListener)
                 UserStore.removeChangeListener(this.changeListener)
-                CustomerStore.removeChangeListener(this.changeListener)
                 
                 delete this.changeListener
             }
         }
 
-        render() {
-            return (
+        render() {			
+			return (
                 <ComposedComponent
                     {...this.props}
                     user = {this.state.user}
-                    customer = {this.state.customer}
-                    billingAddressString = {this.state.billingAddressString}
-                    billingAddress = {this.state.billingAddress}
-                    shippingAddressString = {this.state.shippingAddressString}
-                    shippingAddress = {this.state.shippingAddress}
                     userToken = {this.state.userToken}
                     loggedIn = {this.state.loggedIn} />
             )
