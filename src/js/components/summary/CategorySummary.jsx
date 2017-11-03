e1fd579cc7c113740ec852fd3245dc2801c309b3
Lucas <lucaslopatka@gmail.com>
2017-11-02 21:43:06 -0600

    Summary components.

--- src/js/components/summary/CategorySummary.jsx
+++ src/js/components/summary/CategorySummary.jsx
@@ -9,91 +9,95 @@ import { Button, Checkbox, Radio, } from 'react-bootstrap'
 export default class CategorySummary extends Component {
     render() {
         return (
-            <Row>
-                <Col sm={12}>
-                    <section 
-                        className='summary-component'>
-                        <Row>
-                            <Col md={12} className='col-pinned-9'>
-                                <Row>
-                                    <Col md={3}>
-                                        <div 
-                                            className='media-photo-badge'>
-                                            <Image style={{ margin: '0 auto', maxHeight: '25rem' }} src="image/banners/categories/coffee.jpg" />
-                                        </div>
-                                    </Col>
-                                    <Col md={9}>
-                                        {/*<Row>
-                                            <Col sm={12}>
-                                                <Tabs id='product-tabs-switcher'>
-                                                    <Tab title='Overview' eventKey={1}></Tab>
-                                                    <Tab title='Specifications' eventKey={2}></Tab>
-                                                    <Tab title='Resources' eventKey={3}></Tab>
-                                                </Tabs>
-                                            </Col>
-                                        </Row>*/}
-                                        
-                                        <Row>
-                                            <Col sm={12}>
-                                                <Heading id='listing_name' 
-                                                    tag='h1' 
-                                                    align='center' 
-                                                    style={{ marginTop: '2.5rem' }} 
-                                                    strong>
-                                                    Products & Merchandise
-                                                </Heading>
-                                                <div id='display-address'>
-                                                </div>
-                                            </Col>
-                                        </Row>
-                                        
-                                        {/*<Row>
-                                            <Col sm={3}
-                                                className='summary-icon'>
-                                                <i className='fa fa-large fa-asterisk'/>
-                                            </Col>
-                                            <Col sm={3}
-                                                className='summary-icon'>
-                                                <i className='fa fa-large fa-fire'/>
-                                            </Col>
-                                            <Col sm={3}
-                                                className='summary-icon'>
-                                                <i className='fa fa-large fa-bolt'/>
-                                            </Col>
-                                            <Col sm={3}
-                                                className='summary-icon'>
-                                                <i className='fa fa-large fa-cog'/>
-                                            </Col>
-                                        </Row>
-                                        
-                                        <Row>
-                                            <Col sm={3}
-                                            className='summary-heading'>
-                                                Format
-                                            </Col>
-                                            <Col sm={3}
-                                            className='summary-heading'>
-                                                Roast
-                                            </Col>
-                                            <Col sm={3}
-                                            className='summary-heading'>
-                                                Caffeine Level
-                                            </Col>
-                                            <Col sm={3}
-                                            className='summary-heading'>
-                                                Grind
-                                            </Col>
-                                        </Row>*/}
-                                        
-                                        
-                                    </Col>
-                                </Row>
-                            </Col>
-                        </Row>
-                        
-                    </section>
-                </Col>
-            </Row>
+            <div className="summary entry-summary">
+                {/* Microdata */}
+                {/*<meta itemProp="priceCurrency" content="USD" />
+                <link itemProp="availability" href="http://schema.org/InStock" />*/}
+                <div className="product-details">
+                    <div className="row collapse">
+                      <div className="col-sm-12 col-md-8 top_row" style={{height: 500}}>
+                        <div className="row collapse">
+                          <div className="col-sm-12 columns">
+                            <div className="product_section">
+                              <span className="section_title"></span>
+                              <span itemProp="name" className="description product_title entry-title">My Special Product</span>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row collapse">
+                          <div className="col-sm-12 flavor">
+                            <div className="product_section">
+                              <span className="section_title">Description</span>
+                              <span itemProp="name" className="tags">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non mi mattis, maximus quam sit amet, posuere sapien. Vivamus aliquet ex sed turpis pellentesque tincidunt.</p>
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row collapse">
+                          <div className="col-sm-12 col-md-5 location mid_block" style={{height: 114}}>
+                            <div className="product_section">
+                              <span className="section_title">Location</span>
+                              <span itemProp="name" className="tags">
+                                <span>Sau Paulo, Brazil</span>
+                              </span>
+                            </div>
+                          </div>
+                          <div className="col-sm-12 col-md-7 mid_block" style={{height: 114}}>
+                            <div className="product_section">
+                              <span className="section_title">Attribute 1</span>
+                              <span itemProp="name" className="tags">
+                                <span>Ipsum lorem dolor</span>
+                              </span>
+                            </div>
+                          </div>
+                        </div>{/* .row .collapse */}
+                      </div>
+                      <div className="col-sm-12 col-md-4 featured_image top_row" style={{backgroundSize: 'contain', height: 399}}>
+                        <div className="product_section">
+                          &nbsp;
+                        </div>
+                      </div>
+                    </div>{/* .row */}
+                    <div className="row collapse">
+                      <div className="col-sm-12 col-md-3 type bottom_row" style={{height: 114}}>
+                        <div className="product_section">
+                          <span className="section_title">Attribute 2</span>
+                          <span itemProp="name" className="tags">
+                            <span>Ipsum lorem dolor</span>
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-12 col-md-5 price bottom_row" style={{height: 114}}>
+                        <div className="product_section">
+                          <div className="options">
+                            <div className="selector-wrapper" style={{display: 'none'}}><select className="single-option-selector" data-option="option1" id="productSelect-option-0"><option value="Default Title">Default Title</option></select></div><select name="id" id="productSelect" className="product-single__variants" style={{display: 'none'}}>
+                              <option selected="selected" data-sku="" value={0}>Default Title - $20.00 USD</option>
+                            </select>
+                          </div>
+                          <div className="price_amount">
+                            <span className="section_title">Price / 12oz.</span>
+                            <span className="visually-hidden">Regular price</span>
+                            <span id="ProductPrice" className="h2" itemProp="name">$20.00</span>
+                          </div>
+                          <span className="section_title">Quantity</span>
+                          <div className="quantity">
+                            <span>
+                              <div className="quantity">
+                                <input type="number" id="Quantity" name="quantity" defaultValue={1} min={1} className="input-text qty text quantity-selector" />
+                              </div>
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-sm-12 col-md-4 purchase bottom_row" style={{height: 114}}>
+                        <button className="btn button" style={{display: 'none'}}>ADD TO CART<div className="" /><a className="" data-toggle="modal" data-target="#myModal" style={{display: 'none'}}>Add to Cart</a></button><button type="submit" name="add" id="AddToCart" className="button">
+                          <span id="AddToCartText">Add to Cart</span>
+                        </button>
+                      </div>
+                    </div>{/* .row */}
+                </div>
+            </div>
         )
     }
 }
