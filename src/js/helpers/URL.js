feac7e0d1735b5166f5d9f470451a0a20996b8e3
Lucas Lopatka <lucas.lopatka@interdynamix.com>
2017-12-11 09:52:53 -0700

    Add path-to-regexp and url-parse libs, added static compile helper to UrlHelper class.

--- src/js/helpers/URL.js
+++ src/js/helpers/URL.js
@@ -1,6 +1,9 @@
 /**********************************************************
  * Namespace: QC.Helpers.URL
  **********************************************************/
+import pathToRegexp from 'path-to-regexp'
+import parse from 'url-parse'
+
 export default class UrlHelper {
     static getParams = (query) => {
         let params = {}
@@ -9,7 +12,7 @@ export default class UrlHelper {
         
         query = query || window.location.search.substr(1).split('&')
         
-        if (query === "") {
+        if (query === '') {
             return params
         }
         
@@ -41,4 +44,31 @@ export default class UrlHelper {
         url = url.replace(/\/+$/, '')
         return (appendTrailing) ? url + '/' : url
     }
+	
+	/**
+	 * url-parse instance properties
+	 *
+	 * protocol: The protocol scheme of the URL (e.g. http:).
+	 * slashes: A boolean which indicates whether the protocol is followed by two forward slashes (//).
+	 * auth: Authentication information portion (e.g. username:password).
+	 * username: Username of basic authentication.
+	 * password: Password of basic authentication.
+	 * host: Host name with port number.
+	 * hostname: Host name without port number.
+	 * port: Optional port number.
+	 * pathname: URL path.
+	 * query: Parsed object containing query string, unless parsing is set to false.
+	 * hash: The "fragment" portion of the URL including the pound-sign (#).
+	 * href: The full URL.
+	 * origin: The origin of the URL.
+	 */
+	static compile = (path, args) => {
+		let parsed = parse(path)
+		
+		let compileFn = pathToRegexp.compile(parsed.pathname)
+		parsed.set('pathname', compileFn(args))
+		console.log(parsed.toString())
+		
+		return parsed.toString()
+	}
 }
