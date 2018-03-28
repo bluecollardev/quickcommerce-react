bbe4c90688e0f13b267a19294c6ebf7c0a108e1a
Lucas M. Lopatka <lucas.lopatka@interdynamix.com>
2018-03-28 17:35:18 -0600

    Small change in TextDetailLine.

--- src/js/components/catalog/item/TextDetailLine.jsx
+++ src/js/components/catalog/item/TextDetailLine.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const TextDetailLine = (props) => {
   return (
     <p className='shop-item-details visible-xs' dangerouslySetInnerHTML={{__html: props.description}}></p>
