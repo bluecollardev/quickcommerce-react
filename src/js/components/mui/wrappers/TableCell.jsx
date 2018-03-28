1f4d1d55e59fdfa9ba81229d8cf7bba7f6d2c29e
Lucas M. Lopatka <lucas.lopatka@interdynamix.com>
2018-03-28 08:56:17 -0600

    Update MUI components.

---
+++ src/js/components/mui/wrappers/TableCell.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { TableCell as MuiTableCell } from 'material-ui/Table'
+
+import { muiTableCellStyle as cellStyle } from '../../../styles/MuiStyles.jsx'
+
+// Mui lib uses some pretty stupid defaults, I'll say...
+// Have to wrap the component to kill the styles until I provide a custom theme...
+const TableCell = (props) => {
+  return (
+    <MuiTableCell {...props} style={cellStyle}>
+      {props.children}
+    </MuiTableCell>
+  )
+}
+
+export default TableCell
---
+++ src/js/styles/MuiStyles.jsx
@@ -0,0 +1,12 @@
+const muiTableRowStyle = {
+  height: '1rem',
+  maxHeight: '1rem !important',
+  lineHeight: '1rem'
+}
+
+// Absolutely retarded default styles...
+const muiTableCellStyle = {
+  padding: '0.25rem'
+}
+
+export { muiTableRowStyle, muiTableCellStyle }
