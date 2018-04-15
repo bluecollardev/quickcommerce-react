28bd986deb30558ceac9112251e9c5d3f595078c
Lucas M. Lopatka <lucas.lopatka@interdynamix.com>
2018-04-15 13:53:53 -0600

    Minor change to FlexIconMenu.

--- src/js/components/menu/FlexIconMenu.jsx
+++ src/js/components/menu/FlexIconMenu.jsx
@@ -36,8 +36,6 @@ class FlexIconMenu extends Component {
     return (
       <Row className={className}>
         {this.props.children}
-        {/*<hr className='no_line'/>*/}
-        <div className='borderLine horzMenu' style={{'transformOrigin': '0% 100% 0px', 'transform': 'matrix(1, 0, 0, 1, 0, 0)'}}></div>
       </Row>
     )
   }   
