3eace481241452f1c0162ff2eff93adcee23712e
Lucas Lopatka <lucas.lopatka@interdynamix.com>
2018-04-24 13:05:59 -0600

    Update package.json, add main and module properties.

---
+++ src/js/index.es.js
@@ -0,0 +1,32 @@
+export { default as Account } from './components/account/index.js'
+export { default as Address } from './components/address/index.js'
+export { default as Browser } from './components/browser/index.js'
+export { default as Cart } from './components/cart/index.js'
+export { default as Catalog } from './components/catalog/index.js'
+export { default as Common } from './components/common/index.js'
+export { default as Customer } from './components/customer/index.js'
+export { default as Form } from './components/form/index.js'
+export { default as Gallery } from './components/gallery/index.js'
+export { default as Menu } from './components/menu/index.js'
+export { default as Shop } from './components/shop/index.js'
+export { default as Stepper } from './components/stepper/index.js'
+export { default as Summary } from './components/summary/index.js'
+export { default as User } from './components/user/index.js'
+
+export { default as Actions } from './actions/index.js'
+export { default as Dispatcher } from './dispatcher/index.js'
+export { default as Factory } from './factory/index.js'
+export { default as Helpers } from './helpers/index.js'
+export { default as Layouts } from './layouts/index.js'
+export { default as Modules } from './modules/index.js'
+export { default as Steps } from './steps/index.js'
+export { default as Stores } from './stores/index.js'
+export { default as Services } from './services/index.js'
+
+export {
+  PosComponent,
+  FormComponent,
+  AuthenticatedComponent,
+  AccountComponent,
+  AuthenticatedAccountComponent
+} from './components/index.js'
