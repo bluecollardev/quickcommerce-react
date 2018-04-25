b67cd2a079086f9d1d5483e54507b12bd3db0752
Lucas Lopatka <lucas.lopatka@interdynamix.com>
2018-04-24 20:02:18 -0600

    Last of the module exports. Hopefully that's all!

--- src/js/helpers/index.js
+++ src/js/helpers/index.js
@@ -27,9 +27,9 @@
 //export { default as XhrRequest } from './XhrRequest.js'
 
 import Array from './Array.js'
-import Cordova from './Cordova.js'
+//import Cordova from './Cordova.js'
 import Date from './Date.js'
-import FileSystem from './FileSystem.js'
+//import FileSystem from './FileSystem.js'
 import Flux from './Flux.js'
 import Form from './Form.js'
 import Html from './HTML.js'
@@ -37,28 +37,28 @@ import Image from './Image.js'
 import Intl from './Intl.js'
 import Json from './JSON.js'
 //import KeyboardAccelerators from './KeyboardAccelerators.js'
-import Locale from './Locale.js'
-import Node from './Node.js'
+//import Locale from './Locale.js'
+//import Node from './Node.js'
 import Number from './Number.js'
 import Object from './Object.js'
 import OcRequest from './OcRequest.js'
 import Props from './Props.js'
-import Responsive from './Responsive.js'
-import Rest from './Rest.js'
-import Scroll from './Scroll.js'
-import Selection from './Selection.js'
+//import Responsive from './Responsive.js'
+//import Rest from './Rest.js'
+//import Scroll from './Scroll.js'
+//import Selection from './Selection.js'
 import SpringDate from './SpringDate.js'
 import String from './String.js'
-import Throttle from './Throttle.js'
+//import Throttle from './Throttle.js'
 import Url from './URL.js'
 import Validator from './Validator.js'
-import XhrRequest from './XhrRequest.js'
+//import XhrRequest from './XhrRequest.js'
 
 export default {
   Array,
-  Cordova,
+  //Cordova,
   Date,
-  FileSystem,
+  //FileSystem,
   Flux,
   Form,
   Html,
@@ -66,21 +66,21 @@ export default {
   Intl,
   Json,
   //KeyboardAccelerators,
-  Locale,
-  Node,
+  //Locale,
+  //Node,
   Number,
   Object,
   OcRequest,
   Props,
-  Responsive,
-  Rest,
-  Scroll,
-  Selection,
+  //Responsive,
+  //Rest,
+  //Scroll,
+  //Selection,
   SpringDate,
   String,
-  Throttle,
+  //Throttle,
   Url,
   Validator,
-  XhrRequest
+  //XhrRequest
 }
 
