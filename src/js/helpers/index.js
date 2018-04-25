403abfd1df9080d61e939c6d831c4eecef1eeba3
Lucas Lopatka <lucas.lopatka@interdynamix.com>
2018-04-24 20:32:03 -0600

    More.

--- src/js/helpers/index.js
+++ src/js/helpers/index.js
@@ -30,27 +30,27 @@ import Array from './Array.js'
 //import Cordova from './Cordova.js'
 import Date from './Date.js'
 //import FileSystem from './FileSystem.js'
-import Flux from './Flux.js'
+//import Flux from './Flux.js'
 import Form from './Form.js'
-import Html from './HTML.js'
-import Image from './Image.js'
+//import Html from './HTML.js'
+//import Image from './Image.js'
 //import Intl from './Intl.js'
 //import Json from './JSON.js'
 //import KeyboardAccelerators from './KeyboardAccelerators.js'
 //import Locale from './Locale.js'
 //import Node from './Node.js'
-import Number from './Number.js'
-import Object from './Object.js'
+//import Number from './Number.js'
+//import Object from './Object.js'
 //import OcRequest from './OcRequest.js'
 //import Props from './Props.js'
 //import Responsive from './Responsive.js'
 //import Rest from './Rest.js'
 //import Scroll from './Scroll.js'
 //import Selection from './Selection.js'
-import SpringDate from './SpringDate.js'
-import String from './String.js'
+//import SpringDate from './SpringDate.js'
+//import String from './String.js'
 //import Throttle from './Throttle.js'
-import Url from './URL.js'
+//import Url from './URL.js'
 //import Validator from './Validator.js'
 //import XhrRequest from './XhrRequest.js'
 
@@ -59,27 +59,27 @@ export default {
   //Cordova,
   Date,
   //FileSystem,
-  Flux,
+  //Flux,
   Form,
-  Html,
-  Image,
+  //Html,
+  //Image,
   //Intl,
   //Json,
   //KeyboardAccelerators,
   //Locale,
   //Node,
-  Number,
-  Object,
+  //Number,
+  //Object,
   //OcRequest,
   //Props,
   //Responsive,
   //Rest,
   //Scroll,
   //Selection,
-  SpringDate,
-  String,
+  //SpringDate,
+  //String,
   //Throttle,
-  Url,
+  //Url,
   //Validator,
   //XhrRequest
 }
