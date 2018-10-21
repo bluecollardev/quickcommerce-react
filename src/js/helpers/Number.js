670addc3e9b5fad327afb2366f75e2d53e9a7bfc
Lucas Lopatka <lucas.lopatka@interdynamix.com>
2018-10-20 19:40:01 -0600

    Add inclusive param to NumberHelper.isInRange.

--- src/js/helpers/Number.js
+++ src/js/helpers/Number.js
@@ -15,7 +15,13 @@ class NumberHelper {
     return (b === 0) ? a : NumberHelper.greatestCommonDivisor(b, a % b)
   }
 
-  static isInRange = (number, rangeMin, rangeMax) => {
+  static isInRange = (number, rangeMin, rangeMax, inclusive) => {
+    inclusive = (typeof inclusive === 'boolean') ? inclusive : true
+
+    if (inclusive) {
+      return (Math.min(rangeMin, number, rangeMax) <= number && Math.max(rangeMin, number, rangeMax) >= number)
+    }
+
     return (Math.min(rangeMin, number, rangeMax) < number && Math.max(rangeMin, number, rangeMax) > number)
   }
 }
